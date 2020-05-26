7f35a40a4d7b3cda7fe1e321a76b115c0f032d5a
camila queiroz <queiroz.cami@outlook.com>
2020-05-26 17:13:20 -0300

    Adding the receiver view

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,16 @@
 class DonationsController < ApplicationController
+
+def index
+    @receiver_profiles = current_user.receiver_profiles
+
+    @profile_categories = []
+    @receiver_profiles.each do |profile|
+      @profile_categories << profile.category
+    end
+
+    @profile_categories.each do |category|
+      @donations = Donation.where(category: category)
+    end
+end
+
 end
