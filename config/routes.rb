8d1335505fc95fe72bb480a23d9496c1d03dc3dc
Gustavo Borges <gustborges@gmail.com>
2020-05-30 13:08:48 -0300

    Fixed conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
-  resources :donations, except: [ :edit ] do
-    resources :transfers, only:[ :show, :create ]
-=======
   resources :donations do
-    resources :transfers, only:[ :show ]
->>>>>>> 224fd27255df5574aed2b92f7c66e52032c5b861
+    resources :transfers, only:[ :show, :create ]
   end
   resources :users, only: [ :index ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
