621c8d7b99f7eeb35d5904e00b89827747a3a148
caio fonteles <fontelescaio@gmail.com>
2020-06-03 00:30:02 +0000

    fix signup params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :name, :address, :role, :phone, :description])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :name, :address, :role, :phone, :description])
   end
 
 end
