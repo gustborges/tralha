c41c90c868ce9f4e62949d47a8b3606478278bb5
patriciachedid <patriciachedid@gmail.com>
2020-06-03 20:45:25 -0300

    adding notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :transfers, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_one_attached :photo
+  has_many :notifications
 
   def receiver?
     role == "receiver"
