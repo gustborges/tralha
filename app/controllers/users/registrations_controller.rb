71cca432d113eff2a63912c94cd512190ff02640
camila queiroz <queiroz.cami@outlook.com>
2020-06-05 16:03:54 -0300

    Removing the navbar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  layout 'footerless', only: :new
+  layout 'footerless', only: [ :new, :edit]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
