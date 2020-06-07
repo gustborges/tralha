25805c4cffc32109ca36f9b628d04f9e5cf91130
camila queiroz <queiroz.cami@outlook.com>
2020-06-07 09:57:21 -0300

    Inserindo o type.js na homepage e padronizando as cores

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,13 +24,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { loadDynamicBannerText } from '../components/banner';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
-    initUpdateNavbarOnScroll();
+    loadDynamicBannerText();
 });
