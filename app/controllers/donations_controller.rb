e2831d28f679212e76c5e3985fcf996c907272fe
caio fonteles <fontelescaio@gmail.com>
2020-06-14 13:00:56 +0000

    deleting trash code

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -9,9 +9,9 @@ class DonationsController < ApplicationController
         @profile_categories << profile.category
       current_user.notifications.update_all(read: true)
       end
-      @categories_id = @receiver_profiles.map { |category| category.category_id }
-      @donations = Donation.includes(:user).where('category_id IN (?)', @categories_id)
-      users_ids = User.near([current_user.latitude, current_user.longitude], 3000, :order => :distance).collect{ |a| a.id }
+      categories_id = @receiver_profiles.map { |category| category.category_id }
+      @donations = Donation.includes(:user).where('category_id IN (?)', categories_id)
+      users_ids = User.near([current_user.latitude, current_user.longitude], 100, :order => :distance).collect{ |a| a.id }
       @donations ||= Donation.all.includes(:user)
       @donations = @donations.where(:user_id =>  users_ids).order(created_at: :desc)
     else
